93fb2db6082d558005b3743260f53d6eaee771cf
Lucas Zacarias <lucas.zacarias@conube.com.br>
2020-07-25 02:10:14 -0300

    adicionado modal à adminPage

--- src/views/pages/AdminLogin.js
+++ src/views/pages/AdminLogin.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import { Grid, Typography, Button } from '@material-ui/core';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { Typography, Button } from "@material-ui/core";
 
-import '../../assets/css/admin-login.css'
+import api from "../../services/api";
+import ModalInsertProducts from "../components/ModalInsertProducts";
+
+import "../../assets/css/admin-login.css";
 
 const AdminLogin = () => {
-	return (
-		<div className="login-form">
-			<Typography variant="h6" color="primary" className="loginTitle">login para administração</Typography>
-				<TextField 
-					className="textfield"
-					label="e-mail" 
-					variant="outlined"
-					color="secondary"
-					required
-				/>
-				<TextField 
-					className="textfield"
-					label="senha" 
-					variant="outlined"
-					color="secondary"
-					required
-				/>
-			<Button variant="contained" color="primary">acessar</Button>
-		</div>
-	)
-}
+  const [openModal, setOpenModal] = useState(false);
+  const handleCreateProduct = async (object) => {
+	await api.createProducts(object);
+	setOpenModal(false);
+  };
 
-export default AdminLogin
+  return (
+    <div className="login-form">
+      <Typography variant="h6" color="primary" className="loginTitle">
+        login para administração
+      </Typography>
+      <TextField
+        className="textfield"
+        label="e-mail"
+        variant="outlined"
+        color="secondary"
+        required
+      />
+      <TextField
+        className="textfield"
+        label="senha"
+        variant="outlined"
+        color="secondary"
+        required
+      />
+      <Button variant="contained" color="primary">
+        acessar
+      </Button>
+      <ModalInsertProducts
+        open={openModal}
+        setOpen={setOpenModal}
+        handleCreateProduct={handleCreateProduct}
+      />
+    </div>
+  );
+};
+
+export default AdminLogin;
